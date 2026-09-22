a289d4497af0508a6b3e8a58dc15dcf839495cfb
catthyy <catthyy25@gmail.com>
2026-09-22 21:29:59 +0100

    repetition-2: Person with birthday

--- src/main/java/software/ulpgc/katas/Person.java
+++ src/main/java/software/ulpgc/katas/Person.java
@@ -1,14 +1,22 @@
 package software.ulpgc.katas;
 
+import java.time.LocalDate;
+
 public class Person {
 
     private final String name;
+    private final LocalDate birthday;
 
-    public Person(String name) {
+    public Person(String name, LocalDate birthday) {
         this.name = name;
+        this.birthday = birthday;
     }
 
     public String name() {
         return name;
+    }
+
+    public LocalDate birthday() {
+        return birthday;
     }
 }
