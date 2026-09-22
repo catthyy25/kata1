a5f5cf87384aeeb569a3d3d9b7ea35be6c16f1b9
catthyy <catthyy25@gmail.com>
2026-09-22 20:34:12 +0100

    Person with name

--- src/main/java/software/ulpgc/katas/Person.java
+++ src/main/java/software/ulpgc/katas/Person.java
@@ -1,4 +1,15 @@
 package software.ulpgc.katas;
 
 public class Person {
+
+    private final String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String name() {
+        return name;
+    }
+
 }
