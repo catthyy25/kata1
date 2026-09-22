85ed38dcb8d9ce449f6c69ec03f7ee72310eb2ad
catthyy <catthyy25@gmail.com>
2026-09-22 21:41:06 +0100

    repetition-3: Person with name

--- src/main/java/software/ulpgc/katas/Person.java
+++ src/main/java/software/ulpgc/katas/Person.java
@@ -1,4 +1,14 @@
 package software.ulpgc.katas;
 
 public class Person {
+
+    private final String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String name() {
+        return name;
+    }
 }
