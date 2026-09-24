3920b4397bedc7d7ca810ca76936e566e85e10f5
catthyy <catthyy25@gmail.com>
2026-09-24 17:40:28 +0100

    Main created

---
+++ src/main/java/software/ulpgc/katas/Main.java
@@ -0,0 +1,11 @@
+package software.ulpgc.katas;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    static void main(){
+        Person person = new Person("Lucia", LocalDate.of(2005, 4, 4));
+        System.out.println(person.age());
+    }
+}
